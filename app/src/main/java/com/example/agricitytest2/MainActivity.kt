bf59056c871564f9894b9907acceda5b14cd614d
suss <sussy@suss.com>
2022-10-15 17:24:20 +0100

    Android Plot tentativa 3

--- app/src/main/java/com/example/agricitytest2/MainActivity.kt
+++ app/src/main/java/com/example/agricitytest2/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.agricitytest2.databinding.ActivityMainBinding
 import java.text.FieldPosition
 import java.text.Format
 import java.text.ParsePosition
+import java.util.*
 import kotlin.math.roundToInt
 
 
@@ -20,59 +21,59 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
 
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+        // initialize our XYPlot reference:
+        val plot = binding.plot
 
-            val domainLabels = arrayOf<Number>(1,2,3,4,5,6,7,8,9)
-            val series1Number= arrayOf<Number>(1,4,8,12,16,32,26,29,10,13)
-            val series2Number = arrayOf<Number>(5, 2, 10, 5, 20, 10, 40, 20, 80, 40)
+        // create a couple arrays of y-values to plot:
+        val domainLabels = arrayOf<Number>(1, 2, 3, 6, 7, 8, 9, 10, 13, 14)
+        val series1Numbers = arrayOf<Number>(1, 4, 2, 8, 4, 16, 8, 32, 16, 64)
+        val series2Numbers = arrayOf<Number>(5, 2, 10, 5, 20, 10, 40, 20, 80, 40)
 
-            val series1 : XYSeries = SimpleXYSeries(listOf(* series1Number), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"Series 1")
-            val series2 : XYSeries = SimpleXYSeries(listOf(* series2Number), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"Series 2")
+        // turn the above arrays into XYSeries':
+        // (Y_VALS_ONLY means use the element index as the x value)
+        val series1: XYSeries = SimpleXYSeries(
+            listOf(*series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1"
+        )
+        val series2: XYSeries = SimpleXYSeries(
+            listOf(*series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2"
+        )
 
-            val series1Format = LineAndPointFormatter(Color.BLUE,Color.BLACK,Color.YELLOW,null)
-            val series2Format = LineAndPointFormatter(Color.GREEN,Color.RED,Color.GRAY,null)
+        // create formatters to use for drawing a series using LineAndPointRenderer
+        // and configure them from xml:
+        val series1Format = LineAndPointFormatter(Color.YELLOW, Color.GREEN, Color.BLUE, null)
+        val series2Format = LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null)
 
-
-            series2Format.linePaint.pathEffect = DashPathEffect(
-            floatArrayOf(
-                // always use DP when specifying pixel sizes, to keep things consistent across devices:
+        // add an "dash" effect to the series2 line:
+        series2Format.linePaint.pathEffect = DashPathEffect(
+            floatArrayOf( // always use DP when specifying pixel sizes, to keep things consistent across devices:
                 PixelUtils.dpToPix(20f),
                 PixelUtils.dpToPix(15f)
-            ), 0F)
+            ), 0F
+        )
 
-            val plot = binding.plot
+        // just for fun, add some smoothing to the lines:
+        // see: http://androidplot.com/smooth-curves-and-androidplot/
+        series1Format.interpolationParams =
+            CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal)
+        series2Format.interpolationParams =
+            CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal)
 
-            series1Format.interpolationParams = CatmullRomInterpolator.Params(10,CatmullRomInterpolator.Type.Centripetal)
-
-            plot.addSeries(series1,series1Format)
-            plot.addSeries(series2,series2Format)
-
-            plot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = object : Format(){
-                override fun format(
-                    obj: Any?,
-                    toAppendTo: StringBuffer,
-                    pos: FieldPosition,
-
-                ) : StringBuffer {
-                    val i = (obj as Number).toFloat().roundToInt()
-                    return toAppendTo.append(domainLabels[i])
-
-                }
-
-                override fun parseObject(source: String?, pos: ParsePosition): Any?{
-                    return null
-                }
-
+        // add a new series' to the xyplot:
+        plot.addSeries(series1, series1Format)
+        plot.addSeries(series2, series2Format)
+        plot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = object : Format() {
+            override fun format(
+                obj: Any,
+                toAppendTo: StringBuffer,
+                pos: FieldPosition
+            ): StringBuffer {
+                val i = (obj as Number).toFloat().roundToInt()
+                return toAppendTo.append(domainLabels[i])
             }
 
+            override fun parseObject(source: String, pos: ParsePosition): Any? {
+                return null
+            }
+        }
     }
-
-    public override fun onStart() = super.onStart()
-    public override fun onResume() = super.onResume()
-    public override fun onPause() = super.onPause()
-    public override fun onDestroy() = super.onDestroy()
-    public override fun onRestart() = super.onRestart()
 }
-
-
