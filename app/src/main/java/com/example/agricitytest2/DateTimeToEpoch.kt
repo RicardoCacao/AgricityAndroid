55d6194ae8a1decc1c9b232dca2e1fdad486994f
cacaopr <cacaopr>
2023-01-26 10:52:53 +0000

    Removed some Logs that were flooding logcat

--- app/src/main/java/com/example/agricitytest2/DateTimeToEpoch.kt
+++ app/src/main/java/com/example/agricitytest2/DateTimeToEpoch.kt
@@ -1,6 +1,5 @@
 package com.example.agricitytest2
 
-import android.util.Log
 import java.text.SimpleDateFormat
 
 private const val TAG = "DateTimeToEpoch"
@@ -11,16 +10,14 @@ object DateTimeToEpoch {
     fun dateTimeToEpochLong(dateTime: String): Long {
 
         //Log.d(TAG, "Passed $dateTime")
-        val df = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateTime.toString()).time.toLong()
         //Log.d(TAG, "Returned $df")
-        return  df
+        return SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateTime.toString()).time.toLong()
 
     }
     fun dateTimeToEpochFloat(dateTime: String): Float {
         //Log.d(TAG, "Passed $dateTime")
-        val df =  SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateTime.toString()).time.toFloat()
         //Log.d(TAG, "Returned $df")
-        return  df
+        return SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateTime.toString()).time.toFloat()
 
     }
 
